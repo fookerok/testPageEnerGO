dd4ed85b3d5095304a015917a6948c36d9e6d897
Fookerok <gulenkov.dmitriy@mail.ru>
2025-07-13 01:42:14 +0300

    13072025

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,35 @@
-console.log('dasdsa')
+const btnAdd = document.querySelector('.btn-add');
+const cardsContainer = document.querySelector('.cards-container');
+
+btnAdd.addEventListener('click', () => {
+	const companySelects = document.querySelectorAll('.select');
+	const company = companySelects[0].options[companySelects[0].selectedIndex].text;
+	const job = document.querySelectorAll('.input.job')[0].value;
+	const department = companySelects[1].options[companySelects[1].selectedIndex].text;
+
+	const newCard = document.createElement('div');
+	newCard.className = 'card__company';
+	newCard.innerHTML = `
+		<div class="input__image input__image--company image__card"></div>
+		<div class="data__company">
+		<div class="data-name">${company}</div>
+		<div class="data-job">${job}</div>
+		<div class="data-department">${department}</div>
+		</div>
+		<div class="btn-block">
+		<button class="btn-delete"></button>
+		</div>
+	`;
+
+	newCard.querySelector('.btn-delete').addEventListener('click', () => {
+		newCard.remove();
+	});
+
+	cardsContainer.appendChild(newCard);
+});
+
+document.querySelectorAll('.btn-delete').forEach(btn => {
+	btn.addEventListener('click', () => {
+		btn.closest('.card__company').remove();
+	});
+});
