f04837698c5330399006536aef736ddce3727bf1
Fookerok <gulenkov.dmitriy@mail.ru>
2025-07-13 21:35:22 +0300

    13072025 final desktop mb

--- src/js/script.js
+++ src/js/script.js
@@ -33,3 +33,59 @@ document.querySelectorAll('.btn-delete').forEach(btn => {
 		btn.closest('.card__company').remove();
 	});
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+	function initImageUploader(id) {
+		const input = document.getElementById('imageInput' + id);
+		const previewContainer = document.querySelector('.preview' + id);
+		const previewImage = previewContainer.querySelector('img');
+		const clearBtn = document.querySelector('.clear-img' + id);
+		const uploadButton = document.querySelector('.upload-button' + id);
+
+		input.addEventListener('change', function() {
+			const file = this.files[0];
+			if (!file) return;
+			if (file.type !== 'image/png') {
+				alert('Пожалуйста, загрузите файл в формате PNG.');
+				input.value = '';
+				return;
+			}
+			if (file.size > 1024 * 1024) {
+				alert('Размер файла не должен превышать 1 Мб.');
+				input.value = '';
+				return;
+			}
+			const reader = new FileReader();
+			reader.onload = function(e) {
+				const img = new Image();
+				img.src = e.target.result;
+				img.onload = function() {
+					if (img.width < 600) {
+						alert('Ширина изображения должна быть не менее 600px.');
+						input.value = '';
+						return;
+					}
+					previewImage.src = e.target.result;
+					previewContainer.style.display = 'flex';
+					clearBtn.style.display = 'block';
+					uploadButton.style.display = 'none';
+				};
+			};
+			reader.readAsDataURL(file);
+		});
+		clearBtn.addEventListener('click', function() {
+			input.value = '';
+			previewImage.src = '#';
+			previewContainer.style.display = 'none';
+			clearBtn.style.display = 'none';
+			uploadButton.style.display = 'flex';
+		});
+	}
+
+	initImageUploader(1);
+	initImageUploader(2);
+	initImageUploader(3);
+});
+
+
+
